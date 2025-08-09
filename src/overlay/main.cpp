33a086637ea7840251ce855737efea66d99d400a
Hyblocker <hyblocker@protonmail.com>
2025-08-09 14:35:44 +0200

    (chore): nova rewrite initial commit

---
+++ src/overlay/main.cpp
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <windows.h>
+
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPWSTR lpCmdLine, _In_ int nCmdShow)
+{
+    printf("Hello World!\n");
+    return 0;
+}
