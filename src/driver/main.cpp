872cf888844cbe6f58a7a3e01fb357276e5cf751
Hyblocker <hyblocker@protonmail.com>
2025-11-16 20:17:31 +0100

    chore: Update driver entrypoint

--- src/driver/main.cpp
+++ src/driver/main.cpp
@@ -1,15 +1,19 @@
 #include <stdio.h>
 #include "tracked_device_provider.h"
 
-#ifdef OPENVRSPACECALIBRATORDRIVER_EXPORTS
-#define OPENVRSPACECALIBRATORDRIVER_API extern "C" __declspec(dllexport)
+#if defined( _WIN32 )
+#define SPACECALIBRATORDRIVER_EXPORT extern "C" __declspec( dllexport )
+#define SPACECALIBRATORDRIVER_IMPORT extern "C" __declspec( dllimport )
+#elif defined( __GNUC__ ) || defined( COMPILER_GCC ) || defined( __APPLE__ )
+#define SPACECALIBRATORDRIVER_EXPORT extern "C" __attribute__( ( visibility( "default" ) ) )
+#define SPACECALIBRATORDRIVER_IMPORT extern "C"
 #else
-#define OPENVRSPACECALIBRATORDRIVER_API extern "C" __declspec(dllimport)
+#error "Unsupported Platform."
 #endif
 
 spacecal::ServerTrackedDeviceProvider g_server;
 
-OPENVRSPACECALIBRATORDRIVER_API void* HmdDriverFactory(const char* pInterfaceName, int* pReturnCode)
+SPACECALIBRATORDRIVER_EXPORT void* HmdDriverFactory(const char* pInterfaceName, int* pReturnCode)
 {
     if (std::strcmp(vr::IServerTrackedDeviceProvider_Version, pInterfaceName) == 0) {
         return &g_server;
