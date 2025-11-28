94e67a708a07805807ceb52b64dc8db24359fcbe
Hyblocker <hyblocker@protonmail.com>
2025-11-28 11:52:20 +0100

    feat: Begin implementing calibration alg

--- src/overlay/calibration.cpp
+++ src/overlay/calibration.cpp
@@ -4,12 +4,102 @@
 
 namespace spacecal {
 
+    CalibrationManager* CalibrationManager::s_instance = nullptr;
+
+    // pose -> hmdmatrix
+    Pose_t::Pose_t(const vr::HmdMatrix34_t hmdMatrix) {
+        // [r] [r] [r] [t]
+        // [r] [r] [r] [t]
+        // [r] [r] [r] [t]
+        //    where r is a 3x3 rot matrix, t is a 3 element vector
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                rot(i, j) = hmdMatrix.m[i][j];
+            }
+        }
+        trans = Eigen::Vector3d(hmdMatrix.m[0][3], hmdMatrix.m[1][3], hmdMatrix.m[2][3]);
+    }
+
     void TrackingSystemCalibration::init() {
-        // @TODO:
+        // @TODO: 
+
+    }
+
+    void TrackingSystemCalibration::start() {
+        // @TODO: 
+
+    }
+    
+    void TrackingSystemCalibration::calibrationTick(const double deltaTime) {
+        if (!vr::VRSystem())
+            return;
+
+        if ((deltaTime - m_lastTick) < 0.05)
+            return;
+
+        m_lastTick = deltaTime;
+
+        // original code checks hmd specifically, we should check that the hmd and ref arent at 0 0 0
+        // check that ref is not at origin
+        auto p = m_ipcServer.devicePoses[vr::k_unTrackedDeviceIndex_Hmd].vecPosition;
+        if (hmdIsInSameTrackingSystem) {
+            
+        }
+        m_xPrev = (float)p[0];
+        m_yPrev = (float)p[1];
+        m_zPrev = (float)p[2];
+
+    }
+    
+    void TrackingSystemCalibration::reset() {
+        // @TODO: 
+
+    }
+    
+    void TrackingSystemCalibration::apply() {
+        // @TODO: 
+
+    }
+
+    CalibrationManager::CalibrationManager() {
+        // @FIXME: Need to figure out how to handle multiple calibrations
+        TrackingSystemCalibration mainCalibration;
+        mainCalibration.hmdIsInSameTrackingSystem = true;
+        m_calibrations.push_back(mainCalibration);
     }
 
     void CalibrationManager::init() {
-        // @TODO:
+        // @TODO: 
+
+
+        for (auto& calibration : m_calibrations) {
+            calibration.init();
+        }
     }
 
+    void CalibrationManager::start() {
+        // @TODO: 
+
+
+        for (auto& calibration : m_calibrations) {
+            calibration.start();
+        }
+    }
+
+    void CalibrationManager::calibrationTick(const double deltaTime) {
+        // @TODO: 
+
+
+        for (auto& calibration : m_calibrations) {
+            calibration.calibrationTick(deltaTime);
+        }
+    }
+
+    TrackingSystemCalibration& CalibrationManager::getCalibration(const size_t index) {
+        return m_calibrations[index];
+    }
+
+    size_t CalibrationManager::getCalibrationCount() const {
+        return m_calibrations.size();
+    }
 }
--- src/overlay/calibration.h
+++ src/overlay/calibration.h
@@ -1,40 +1,127 @@
 #pragma once
 
+#include "protocol.h"
 #include "vr_core.h"
 #include <Eigen/Dense>
 
+// forward decl
+namespace ipc {
+    class IpcClient;
+}
+
 namespace spacecal {
+
+    enum class CalibrationState {
+        // calibration is inactive
+        NONE,
+        // calibration is starting
+        START,
+        // collecting samples
+        SAMPLE,
+        // calibrating orientation
+        ROTATION,
+        // calibrating position
+        TRANSLATION,
+        // calibrating scale
+        SCALE,
+        // user is editing calibration
+        EDITING,
+        // collecting samples for continuous
+        CONTINUOUS_SAMPLE,
+        // continuous calibration, but idle
+        CONTINUOUS_IDLE,
+    };
+
+    // numeric value corresponds to the sample count for the given speed, the enum is just "naming" and avoids expensive lookup :3
+    enum class CalibrationSpeed {
+        FAST = 100,
+        SLOW = 250,
+        VERY_SLOW = 500,
+    };
 
     // An instantaneous pose, SteamVR poses are mapped to this for ease of use with
     // Eigen
     struct Pose_t {
         Eigen::Matrix3d rot;
         Eigen::Vector3d trans;
+        Pose_t(const vr::HmdMatrix34_t hmdMatrix);
     };
 
     // An instantaneous sample. Several of these are collected and passed into the
     // Calibration algorithm to create a mapping from the reference device to the
     // target device.
     struct Sample_t {
-        Pose_t reference;
-        Pose_t target;
-        double timestamp;
+        Pose_t reference; // what we are calibrating TO
+        Pose_t target;    // the object that shall be calibrated
+        double timestamp; // @FIXME: seems to be unused with existing algorithm?
     };
 
-    // An instance of the calibration algoritm, that handles collecting samples, and
+    struct CalibrationDevice {
+        vr::TrackedDeviceIndex_t deviceId = vr::k_unTrackedDeviceIndexInvalid;
+        std::string trackingSystem;
+        std::string deviceModel;
+        std::string deviceSerialNumber;
+    };
+
+    class CalibrationManager;
+
+    // An instance of the calibration algorithm, that handles collecting samples, and
     // deducing a mapping between calibrations of two tracking systems. It is
     // separated from the Calibration Manager to allow one to generate multiple
     // calibrations for multiple combinations of playspaces.
     class TrackingSystemCalibration {
     public:
         void init();
-    };
+        void start();
+        void calibrationTick(const double deltaTime);
+        void reset();
+        // applies the calibration to the VR runtime
+        void apply();
 
-    class CalibrationManager {
-    public:
-        void init();
+        bool isActive = false; // enabled in the UI
+        bool isValidCalibration = false; // whether we can even use this calibration
+        bool hmdIsInSameTrackingSystem = false; // whether the hmd is part of the tracking systems involved in this calibration
+        CalibrationSpeed calibrationSpeed = CalibrationSpeed::FAST;
+        CalibrationState state = CalibrationState::NONE;
+
+        CalibrationDevice referenceDevice; // what we are calibrating to (ie this will become the ABSOLUTE ORIGIN)
+        CalibrationDevice targetDevice; // what we are calibrating (ie this tracking system will be manipulated to match the ref)
+
+        // the calibrated pose
+        Eigen::Vector3d calibratedRotation;
+        Eigen::Vector3d calibratedTranslation;
+        double calibratedScale;
 
     private:
+        double m_lastTick;
+        std::vector<Sample_t> m_samples;
+
+        friend class CalibrationManager;
+    };
+
+    // Global manager for calibrations, once instance per VR session
+    class CalibrationManager {
+    public:
+        explicit CalibrationManager();
+        void init();
+        void start(); // how do i even define a calibration????
+
+        // handles updating pending calibrations every frame
+        void calibrationTick(const double deltaTime);
+
+        // @TODO: Better getter? idk how a calibration should be defined in terms of ui
+        TrackingSystemCalibration& getCalibration(const size_t index);
+        size_t getCalibrationCount() const;
+
+        [[nodiscard]] static inline CalibrationManager* getInstance() { return s_instance; }
+
+    private:
+
+        vr::DriverPose_t m_poses[vr::k_unMaxTrackedDeviceCount] = {};
+        std::vector<TrackingSystemCalibration> m_calibrations;
         VRState m_vrState;
+
+        static CalibrationManager* s_instance;
+        friend class ::ipc::IpcClient;
     };
 } // namespace spacecal
