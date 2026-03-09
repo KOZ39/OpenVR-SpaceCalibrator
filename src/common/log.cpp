26c2ce677960050a92f2e03a02c169b986e27ade
Hyblocker <hyblocker@protonmail.com>
2026-03-10 00:03:36 +0100

    fix: incorrect logger string in overlay and driver

--- src/common/log.cpp
+++ src/common/log.cpp
@@ -187,13 +187,13 @@ namespace logging {
         DeleteOldLogFiles(util::getSpaceCalibratorLogsDir());
 
         if (isOverlay) {
-            s_logger = MAKE_LOGGER_OVERLAY("driver");
+            s_logger = MAKE_LOGGER_OVERLAY("overlay");
             s_loggerOpenVr = MAKE_LOGGER_OVERLAY("openvr");
             s_loggerIpc = MAKE_LOGGER_OVERLAY("ipc");
             s_loggerHooking = MAKE_LOGGER_OVERLAY("hooking");
             s_loggerCalibration = MAKE_LOGGER_OVERLAY("calibration");
         } else {
-            s_logger = MAKE_LOGGER_DRIVER("overlay");
+            s_logger = MAKE_LOGGER_DRIVER("driver");
             s_loggerOpenVr = MAKE_LOGGER_DRIVER("openvr");
             s_loggerIpc = MAKE_LOGGER_DRIVER("ipc");
             s_loggerHooking = MAKE_LOGGER_DRIVER("hooking");
