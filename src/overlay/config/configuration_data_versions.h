1a8350d8991f0e7a723f3d9c7a8125af3c7617b4
Hyblocker <hyblocker@protonmail.com>
2026-04-01 01:13:57 +0200

    fix: default values

--- src/overlay/config/configuration_data_versions.h
+++ src/overlay/config/configuration_data_versions.h
@@ -25,52 +25,51 @@ namespace spacecal {
                 };
 
                 struct LegacyAlignmentParams {
-                    double align_speed_large;
-                    double align_speed_small;
-                    double align_speed_tiny;
-                    float continuousCalibrationThreshold;
-                    double thr_rot_large;
-                    double thr_rot_small;
-                    double thr_rot_tiny;
-                    double thr_trans_large;
-                    double thr_trans_small;
-                    double thr_trans_tiny;
+                    double align_speed_large = 0.0;
+                    double align_speed_small = 0.0;
+                    double align_speed_tiny = 0.0;
+                    float continuousCalibrationThreshold = 0.0f;
+                    double thr_rot_large = 0.0;
+                    double thr_rot_small = 0.0;
+                    double thr_rot_tiny = 0.0;
+                    double thr_trans_large = 0.0;
+                    double thr_trans_small = 0.0;
+                    double thr_trans_tiny = 0.0;
                 } alignmentParams;
 
-                bool autostart_continuous_calibration;
-                bool ignore_outliers;
-                bool lock_relative_position;
-                bool quash_target_in_continuous; // hide tracker (why was it called quash in
-                // legacy??)
-                bool relative_pos_calibrated;
-                bool require_trigger_press_to_apply;
-                bool static_calibration;
-                uint32_t calibration_speed; // enum: FAST: 0, SLOW: 1, VERY_SLOW: 2
-                double continuous_calibration_target_offset_x;
-                double continuous_calibration_target_offset_y;
-                double continuous_calibration_target_offset_z;
+                bool autostart_continuous_calibration = false;
+                bool ignore_outliers = false;
+                bool lock_relative_position = false;
+                bool quash_target_in_continuous = false; // hide tracker (why was it called quash in legacy??)
+                bool relative_pos_calibrated = false;
+                bool require_trigger_press_to_apply = false;
+                bool static_calibration = false;
+                uint32_t calibration_speed = 0; // enum: FAST: 0, SLOW: 1, VERY_SLOW: 2
+                double continuous_calibration_target_offset_x = 0.0;
+                double continuous_calibration_target_offset_y = 0.0;
+                double continuous_calibration_target_offset_z = 0.0;
 
-                int jitter_threshold;
-                float max_relative_error_threshold;
+                int jitter_threshold = 0;
+                float max_relative_error_threshold = 0.0;
                 LegacyTrackingDevice reference_device;
                 std::string reference_tracking_system;
                 struct LegacyRelativeTransform {
-                    double x;
-                    double y;
-                    double z;
-                    double yaw;
-                    double pitch;
-                    double roll;
+                    double x = 0.0;
+                    double y = 0.0;
+                    double z = 0.0;
+                    double yaw = 0.0;
+                    double pitch = 0.0;
+                    double roll = 0.0;
                 } relative_transform;
-                double scale;
+                double scale = 1.0;
                 LegacyTrackingDevice target_device;
                 std::string target_tracking_system;
-                double x;
-                double y;
-                double z;
-                double yaw;
-                double roll;
-                double pitch;
+                double x = 0.0;
+                double y = 0.0;
+                double z = 0.0;
+                double yaw = 0.0;
+                double roll = 0.0;
+                double pitch = 0.0;
             };
 
             struct Configuration_0 {
@@ -87,8 +86,8 @@ namespace spacecal {
                     std::string tracking_system;
                 };
                 struct Transform {
-                    float x, y, z; // pos
-                    float yaw, pitch, roll; // rot
+                    float x = 0.0f, y = 0.0f, z = 0.0f; // pos
+                    float yaw = 0.0f, pitch = 0.0f, roll = 0.0f; // rot
                 };
 
                 bool isActive = false;
